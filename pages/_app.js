186ba73a3bd25756026a98afc6d15d5a750fe4a2
viveknimbolkar <viveknimbolkar50@gmail.com>
2024-04-03 02:54:51 +0530

    maintainance

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,17 @@ import React from "react";
 import { Toaster } from "react-hot-toast";
 export default function App({ Component, pageProps }) {
   return (
-    <AccountProvider>
-      <main>
-        <Component {...pageProps} />
-        <Toaster position="bottom-left" />
-      </main>
-    </AccountProvider>
+    // <AccountProvider>
+    //   <main>
+    //     <Component {...pageProps} />
+    //     <Toaster position="bottom-left" />
+    //   </main>
+    // </AccountProvider>
+    <div className="h-screen w-full flex items-center justify-center">
+      <h1 className="text-3xl text-center">
+        We are under maintainance. <br />
+        We will back soon :-{"( "}
+      </h1>
+    </div>
   );
 }
